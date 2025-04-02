1f2ce27ff78a9b3bf6ed9ed9039a6af6a98e0c14
Gwaruingi <gwaruingi@gmail.com>
2025-04-02 07:25:59 +0300

    Fix TypeScript error in auth.ts by properly importing and using Mongoose Document type

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ import { User } from "@/models/User";
 import bcrypt from "bcryptjs";
 import { MongoClient } from "mongodb";
 import { Adapter } from "next-auth/adapters";
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 // Test MongoDB connection - using a separate client to avoid topology issues
 const testConnection = async () => {
@@ -125,7 +125,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       if (user) {
         token.role = user.role;
         // Convert MongoDB ObjectId to string
-        token.id = (user as mongoose.Document)._id.toString();
+        token.id = (user as Document)._id.toString();
       }
       return token;
     },
